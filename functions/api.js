0814d0366e1c5641fc061cdf17d7078034c8afb4
Prity25-coder <pritynishad480@gmail.com>
2025-03-26 19:19:50 +0530

    modify file

--- functions/api.js
+++ functions/api.js
@@ -1,4 +1,5 @@
-import serverless from "serverless-http"
-import server from "../"
+import serverless from "serverless-http";
+import { server, io } from "../";
 
 export const handler = serverless(server);
+export const handler2 = serverless(io);
